d4e8553e9dba273338a381f9a046c6f3c3d88e53
EloxZ <45355879+EloxZ@users.noreply.github.com>
2022-01-20 20:50:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.set('views', path.join(__dirname, 'views')); // Para definir la ruta de las 
 
 // Middlewares
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 //Configuracion de la base de datos mongo
