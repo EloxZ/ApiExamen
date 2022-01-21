ac58ffef57ff32646d1802f150dd0f754c907e58
EloxZ <eloyway@gmail.com>
2022-01-21 19:34:44 +0100

    Update pujas.js

--- routes/pujas.js
+++ routes/pujas.js
@@ -2,7 +2,7 @@ module.exports = function (app, gestorBD) {
     
     app.get("/pujas", function (req, res) {
         gestorBD.obtenerItem({}, 'pujas', function (pujas) {
-            if (articulos == null) {
+            if (pujas == null) {
                 res.send({ Error: { status: 500, data: "Se ha producido un error al obtener la lista de pujas, intentelo de nuevo más tarde" } })
             } else {
                 res.send({status: 200, data: {pujas: pujas}});
@@ -14,7 +14,7 @@ module.exports = function (app, gestorBD) {
         //TODO hacer validador y encriptar la contraseña
         console.log(req.body);
         gestorBD.insertarItem(req.body, 'pujas', function (puja) {
-            if (articulo == null) {
+            if (puja == null) {
                 res.send({ Error: { status: 500, data: "Se ha producido un error al insertar la puja, intentelo de nuevo más tarde" } })
             }
             else {
